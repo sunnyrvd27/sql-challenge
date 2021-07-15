c3193a4cd6e638e26c5bf1ab80ae80200eab812e
sunnyrvd27 <sunnyrvd@gmail.com>
2021-07-15 16:00:17 -0700

    Next commit for the SQL Assignment after creating Data Analysis Query 6

---
+++ EmployeeSQL/DataAnalysisQuery6.sql
@@ -0,0 +1,45 @@
+-----------------------------------------------------------------------------------------------------------------
+-- List all employees in the Sales department, including their employee number, 
+-- last name, first name, and department name.
+-----------------------------------------------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------------------------------------------
+-- We will use the employees, dept_emp and departments tables to fetch
+-- the desired results
+------------------------------------------------------------------------------------------------
+
+SELECT	em.emp_no		AS "Employee Number",
+		em.last_name	AS "Employee Last Name",
+		em.first_name 	AS "Employee First Name",
+		dp.dept_name	AS "Employee Department"
+FROM	employees	em,
+		dept_emp	de,
+		departments	dp
+WHERE	em.emp_no		= de.emp_no
+AND		de.dept_no		= dp.dept_no
+AND		dp.dept_name 	= 'Sales'
+ORDER BY em.emp_no;
+
+-----------------------------------------------------------------------------
+-- 52245 rows fetched
+-----------------------------------------------------------------------------
+
+-----------------------------------------------------------------------------
+-- FETCHING RESULTS USING JOINS
+-----------------------------------------------------------------------------
+
+SELECT	em.emp_no		AS "Employee Number",
+		em.last_name	AS "Employee Last Name",
+		em.first_name 	AS "Employee First Name",
+		dp.dept_name	AS "Employee Department"
+FROM	employees	em
+JOIN	dept_emp	de
+ON		em.emp_no		= de.emp_no
+JOIN	departments	dp
+ON		de.dept_no		= dp.dept_no
+WHERE	dp.dept_name 	= 'Sales'
+ORDER BY em.emp_no;
+
+-----------------------------------------------------------------------------
+-- 52245 rows fetched
+-----------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -6,14 +6,19 @@
 
 # The data CSV files are added in the data folder. 
 
-# DataAnalysisQuery1.sql created for List the following details of each # employee: employee number, last name, first name, sex, and salary.
+# DataAnalysisQuery1.sql created to List the following details of each # employee: employee number, last name, first name, sex, and salary.
 
-# DataAnalysisQuery2.sql created for List first name, last name, and # hire date for employees who were hired in 1986.
+# DataAnalysisQuery2.sql created to List first name, last name, and # hire date for employees who were hired in 1986.
 
-# DataAnalysisQuery3.sql created for List the manager of each # department with the following information: department number, # department name, the manager's employee number, last name, first # name.
+# DataAnalysisQuery3.sql created to List the manager of each  
+# department with the following information: department number, 
+# department name, the manager's employee number, last name, first 
+# name.
 
-# DataAnalysisQuery4.sql created for List the department of each 
+# DataAnalysisQuery4.sql created to List the department of each 
 # employee with the following information: employee number, last name, # first name, and department name.
 
-# DataAnalysisQuery5.sql created for List first name, last name, and   
+# DataAnalysisQuery5.sql created to List first name, last name, and   
 # sex for employees whose first name is "Hercules" and last names begin # with "B."
+
+# DataAnalysisQuery6.sql created to List all employees in the Sales # department, including their employee number, last name, first name, # and department name.
